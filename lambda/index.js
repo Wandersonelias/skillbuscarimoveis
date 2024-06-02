ef8765b17c21e0fad251c4c5457e949a3a4dd506
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-06-02 02:30:01 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -31,7 +31,7 @@ const ImoveisCidadeIntentHandler = {
        
         return axios.get(`https://api.wandersonelias.com.br/alexa/imoveis/${localizado}`).then(function (response) {
                 
-                response.forEach(index => {
+                response.data.forEach(index => {
                         const speakOutput = `Temos um excelente imóvel localizado na ${response.data[index].endereco} no bairro, ${response.data[index].bairro} uma ótima opção de ${response.data[index].tipo} no valor de R$ ${response.data[index].valor}`;
                         handlerInput.responseBuilder
                         .speak(speakOutput)
