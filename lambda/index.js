cdcaf5b7e13078cb9b1b44528dafd85faf73ec0d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-06-11 01:41:12 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -281,7 +281,7 @@ const AgendamentosIntentHandler = {
         const response = await axios.post(`https://api.wandersonelias.com.br/alexa/agendamentos`,data,{});
         const imovel = response.data
         
-        const speakOutput = `Seu agendamento foi criada em nome de  ${nomeusuario} no dia ${data} para as ${hora} no imóvel código ${imovelId}`;
+        const speakOutput = `Seu agendamento foi criada em nome de  ${nomeusuario} no dia ${data} para as ${hora} no imóvel código ${imovelId}, retornaremos o contato para detalhes, muito obrigado! `;
         return handlerInput.responseBuilder.speak(speakOutput).getResponse();
     } catch(error){
       
