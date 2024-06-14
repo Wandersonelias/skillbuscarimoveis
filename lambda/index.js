d7f483718bdc12cbc0252351e770907c14318df5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-06-14 00:39:16 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -406,8 +406,6 @@ const ErrorHandler = {
 };*/
 
 
-
-
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
